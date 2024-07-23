07997ac2987a7756adb370a1d82ba2cf92d687ec
Riquelme <riquelmepantojaf@gmail.com>
2024-07-23 00:13:13 -0300

    formulário conexão

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-
+// Efeito de menu ativo com rolagem
 let links = document.querySelectorAll(".js-link");
 let sections = document.querySelectorAll(".section");
 window.addEventListener('scroll', () => {
@@ -34,3 +34,25 @@ function scrollSection(event) {
 links.forEach(link => {
     link.addEventListener('click', scrollSection)
 });
+
+// Menu hamburguer mobile
+const btnMobile = document.getElementById("btn-mobile");
+
+function toogleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault()
+    const nav = document.getElementById("nav");
+    nav.classList.toggle('ative');
+    const ative = nav.classList.contains('ative')
+    event.currentTarget.setAttribute('aria-expanded', ative)
+    if (ative) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
+    } else
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
+
+}
+btnMobile.addEventListener('click', toogleMenu);
+btnMobile.addEventListener('touchstart', toogleMenu);
+
+
+// Formulário
+const formulario = document.querySelector('form');
